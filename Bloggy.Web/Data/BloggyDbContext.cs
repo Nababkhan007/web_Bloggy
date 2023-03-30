c8f20fed10a9c19684fc5f1679ca1e3bbeb3d709
Nababkhan007 <markohenry46@gmail.com>
2023-03-30 09:42:10 +0600

    Add properties in BloggyDbContext class

--- Bloggy.Web/Data/BloggyDbContext.cs
+++ Bloggy.Web/Data/BloggyDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Bloggy.Web.Data
+﻿using Bloggy.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggy.Web.Data
 {
-    public class BloggyDbContext
+    public class BloggyDbContext : DbContext
     {
+        public BloggyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
     }
 }
